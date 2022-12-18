981b1170960867cc1a3e5fd921c6f918210c9f0c
Duaa-Badaha <duaabadaha@gmail.com>
2022-12-17 16:40:35 -0800

    Admin activities

---
+++ app/src/main/java/com/example/androidproject/Recipe.java
@@ -0,0 +1,44 @@
+package com.example.androidproject;
+
+
+public class Recipe {
+    public String name;
+    public String description;
+    public int imageID;
+
+    public static final Recipe[] recipes = {
+            new Recipe(R.drawable.bancake,"Pancake", "Fluffy pancake with honey & butter" )
+    };
+
+    public Recipe(int imageID, String name, String description){
+        this.name = name;
+        this.description = description;
+        this.imageID = imageID;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getImage(){
+        return imageID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setImageID(int imageID) {
+        this.imageID = imageID;
+    }
+
+    @Override
+    public String toString(){return name;}
+}
---
+++ app/src/main/java/com/example/androidproject/RecipesDetails.java
@@ -0,0 +1,30 @@
+package com.example.androidproject;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecipesDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes_details);
+//
+//        Intent intent = getIntent();
+//        int id = (int)intent.getExtras().get("drink_id");
+//
+//        Recipe drink = Recipe.recipes[id];
+//
+//        ImageView image = (ImageView) findViewById(R.id.imgPancake);
+//        image.setImageResource(drink.getImageID());
+//
+//        TextView txtName= (TextView)findViewById(R.id.txtName);
+//        TextView txtDesc= (TextView)findViewById(R.id.txtDesc);
+//
+//        txtName.setText(drink.getName());
+//        txtDesc.setText(drink.getDescription());
+
+    }
+}
