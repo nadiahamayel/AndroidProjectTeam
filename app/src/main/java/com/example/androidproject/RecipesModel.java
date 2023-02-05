cfc0dea7c4927317013e666b5b41d081872ca982
Oraibnm <oraibnidal@gmail.com>
2023-02-05 20:22:15 +0200

    oraib2-5

---
+++ app/src/main/java/com/example/androidproject/RecipesModel.java
@@ -0,0 +1,60 @@
+package com.example.androidproject;
+
+import java.util.Arrays;
+
+public class RecipesModel {
+
+    private String recipe_name;
+    private String recipe_time;
+    private String recipe_description ;
+    private  String [] recipe_category;
+
+    public RecipesModel(String recipe_name, String recipe_time, String recipe_description, String[] recipe_category) {
+        this.recipe_name = recipe_name;
+        this.recipe_time = recipe_time;
+        this.recipe_description = recipe_description;
+        this.recipe_category = recipe_category;
+    }
+
+    public String getRecipe_name() {
+        return recipe_name;
+    }
+
+    public void setRecipe_name(String recipe_name) {
+        this.recipe_name = recipe_name;
+    }
+
+    public String getRecipe_time() {
+        return recipe_time;
+    }
+
+    public void setRecipe_time(String recipe_time) {
+        this.recipe_time = recipe_time;
+    }
+
+    public String getRecipe_description() {
+        return recipe_description;
+    }
+
+    public void setRecipe_description(String recipe_description) {
+        this.recipe_description = recipe_description;
+    }
+
+    public String[] getRecipe_category() {
+        return recipe_category;
+    }
+
+    public void setRecipe_category(String[] recipe_category) {
+        this.recipe_category = recipe_category;
+    }
+
+    @Override
+    public String toString() {
+        return "RecipesModel{" +
+                "recipe_name='" + recipe_name + '\'' +
+                ", recipe_time='" + recipe_time + '\'' +
+                ", recipe_description='" + recipe_description + '\'' +
+                ", recipe_category=" + Arrays.toString(recipe_category) +
+                '}';
+    }
+}
